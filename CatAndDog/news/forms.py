489dc0d4950583af5a232498bbbbc2b823e78f91
SerEgo <Frolov27101993@yandex.ru>
2025-10-22 15:27:54 +0300

    изменение поля содержания в PostForm и изменение verbose_name в models

--- CatAndDog/news/forms.py
+++ CatAndDog/news/forms.py
@@ -1,11 +1,11 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Post, Comment
 
 
-class PostForm(ModelForm):
+class PostForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
-        super(ModelForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.fields['category'].empty_label = 'Выберите категорию'
 
     class Meta:
@@ -17,9 +17,18 @@ class PostForm(ModelForm):
             'video',
             'category',
         ]
+        # имена для полей в форме(но я уже написал в моделях)
+        # labels = {
+        #     'title': '',
+        #     'text': '',
+        #     'category': '',
+        # }
+        widgets = {
+            'text': forms.Textarea(attrs={'class': 'form-textarea', 'rows': 6, })
+        }
 
 
-class CommentForm(ModelForm):
+class CommentForm(forms.ModelForm):
     model = Comment
     fields = ['text']
 
