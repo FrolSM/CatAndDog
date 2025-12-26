1da710f13beefac9a3c2e101d85e6089a49fd193
Frol69 <Frolov27101993@yandex.ru>
2025-12-26 18:39:55 +0300

    смена lookup_expr для фильтрации на icontains

--- news/filters.py
+++ news/filters.py
@@ -3,8 +3,8 @@ from .models import Post, Category
 
 
 class PostFilter(FilterSet):
-    title = CharFilter(label='Заголовок', lookup_expr='iregex')
-    text = CharFilter(label='Содержание поста', lookup_expr='iregex')
+    title = CharFilter(label='Заголовок', lookup_expr='icontains')
+    text = CharFilter(label='Содержание поста', lookup_expr='icontains')
     category = ModelChoiceFilter(queryset=Category.objects.all(), label='Категории', empty_label='Все категории')
 
     # class Meta:
