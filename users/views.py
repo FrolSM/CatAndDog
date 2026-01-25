238eb09222598db0242f062518f4e9cf2bb3e96c
FrolSM <Frolov27101993@yandex.ru>
2026-01-25 21:14:04 +0300

    удаление неиспользуемого метода

--- users/views.py
+++ users/views.py
@@ -4,8 +4,3 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'users/profile.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['is_not_authors'] = not self.request.user.groups.filter(name='authors').exists()
-        return context
