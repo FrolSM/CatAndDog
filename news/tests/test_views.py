e33d229e152f7fe08307cb65af111843ec5af43a
FrolSM <Frolov27101993@yandex.ru>
2026-01-26 16:49:37 +0300

    добавление теста проверки детальной страницы и обработки 404

--- news/tests/test_views.py
+++ news/tests/test_views.py
@@ -10,3 +10,22 @@ def test_posts_list_view(client):
     response = client.get(url)
     assert response.status_code == 200
     assert 'posts' in response.context
+
+@pytest.mark.django_db
+def test_post_detail_view(client):
+    """Проверяем детальную страницу поста и обработку 404"""
+    user = User.objects.create_user(username='author', password='12345')
+    category = Category.objects.create(name='News')
+    post = Post.objects.create(title='Test Post', slug='test-post',
+                               author=user, category=category, text='abc', is_published=True)
+
+    # Существующий пост
+    url = reverse('post_detail', kwargs={'slug': post.slug})
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.context['post'] == post
+
+    # Несуществующий пост
+    url = reverse('post_detail', kwargs={'slug': 'nonexistent'})
+    response = client.get(url)
+    assert response.status_code == 404
