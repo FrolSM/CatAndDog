05490bc726632da40fd6c520935fb9f9f3675606
FrolSM <Frolov27101993@yandex.ru>
2026-01-28 16:13:52 +0300

    добавление тестов форм

---
+++ news/tests/test_forms.py
@@ -0,0 +1,15 @@
+import pytest
+from news.forms import PostForm, CommentForm
+
+@pytest.mark.django_db
+def test_post_form_valid(category):
+    form = PostForm(data={
+        'title': 'Form title',
+        'text': 'Form text',
+        'category': category.id,
+    })
+    assert form.is_valid()
+
+def test_comment_form_valid():
+    form = CommentForm(data={'text': 'Nice!'})
+    assert form.is_valid()
