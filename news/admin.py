253a4b1f05644298169503a59969a02364583c4b
FrolSM <Frolov27101993@yandex.ru>
2026-02-09 19:57:38 +0300

    Refactor: Django admin best practices

--- news/admin.py
+++ news/admin.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.contrib import admin, messages
 from django.utils.safestring import mark_safe
 
@@ -6,55 +5,65 @@ from .models import Post, Category, Pets, Comment, PostMedia
 
 
 class PostMediaInline(admin.TabularInline):
+    """
+    Inline позволяет добавлять фото и видео
+    прямо на странице редактирования поста
+    """
     model = PostMedia
     extra = 1
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',)}
-    fields = ['title', 'text', 'category','post_photo_video']  # поля формы создания и редакции
-    list_display = ('id', 'title', 'post_photo_video', 'is_published', 'category')  # поля отображаемые в списке объектов
+    """
+    Админка для модели Post
+    """
+    fields = ('title', 'text', 'category')  # поля формы создания и редакции
+    list_display = ('id', 'title', 'media_preview', 'is_published', 'category')  # поля отображаемые в списке объектов
     list_display_links = ('id', 'title')  # поля линк
     ordering = ['-time', 'title']  # сортировка
     list_editable = ('is_published',)  # разрешение редактировать прям на странице списка
     list_per_page = 10  # пагинация
-    actions = ['set_published', 'set_draft']  # доп действия
-    list_filter = ['category', 'is_published']  # фильтрация по полям
-    readonly_fields = ('post_photo_video', 'author')  # поля неизменяемое (видное в форме)
-    inlines = [PostMediaInline]
+    actions = ('set_published', 'set_draft')  # доп действия
+    list_filter = ('category', 'is_published')  # фильтрация по полям
+    readonly_fields = ('media_preview',)  # поля неизменяемое (видное в форме)
+    inlines = (PostMediaInline,)
+    exclude = ('author',)
 
     def save_model(self, request, obj, form, change):
+        """
+            Назначаем автора автоматически
+            только при создании поста
+            """
         if not change:  # Только при создании новой записи
             obj.author = request.user
         super().save_model(request, obj, form, change)
 
-    def get_form(self, request, obj=None, **kwargs):
-        # Получаем стандартную форму
-        form = super().get_form(request, obj, **kwargs)
-        if 'author' in form.base_fields:
-            form.base_fields['author'].disabled = True
-        # Явно добавляем поле slug, даже если editable=False
-        form.base_fields['slug'] = forms.CharField(
-            required=False,
-            widget=forms.HiddenInput()
-        )
-        return form
-
     @admin.display(description='Медиа')
-    def post_photo_video(self, post: Post):
-        media_html = ''
-        for media in post.media.all():
-            if media.media_type == 'photo':
-                media_html += f'<img src="{media.file.url}" width=100 style="margin:2px;">'
-            else:
-                media_html += (
-                    f'<video width="150" height="100" controls style="margin:2px;">'
-                    f'<source src="{media.file.url}" type="video/mp4">'
-                    'Ваш браузер не поддерживает видео.'
-                    '</video>'
+    def media_preview(self, obj):
+        """
+        Отображение фото и видео в админке
+        """
+        html = []
+
+        for media in obj.media.all():
+            if media.media_type == PostMedia.MediaType.PHOTO:
+                html.append(
+                    f'<img src="{media.file.url}" '
+                    f'width="100" style="margin:5px;" />'
                 )
-        return mark_safe(media_html or 'Медиа отсутствует')
+            else:
+                html.append(
+                    f'''
+                            <video width="160" height="100" controls style="margin:5px;">
+                                <source src="{media.file.url}" type="video/mp4">
+                                Ваш браузер не поддерживает видео
+                            </video>
+                            '''
+                )
+
+        return mark_safe(''.join(html) or 'Нет медиа')
+
 
     @admin.action(description='Опубликовать выбранные записи')
     def set_published(self, request, queryset):
@@ -73,5 +82,13 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name')
 
 
-admin.site.register(Pets)
-admin.site.register(Comment)
+@admin.register(Pets)
+class PetsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age')
+    search_fields = ('name',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'post', 'user', 'time_comm')
+    list_filter = ('post',)
