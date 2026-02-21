97cd796c736fe14b05c92013b9a40c217d93f126
FrolSM <Frolov27101993@yandex.ru>
2026-02-21 19:03:12 +0300

    изменение адреса профиля

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from .views import IndexView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='profile'),
+    path('profile/', IndexView.as_view(), name='profile'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
