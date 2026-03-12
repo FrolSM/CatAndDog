5b50deab7d764b07b8a16aae2b2be67ad7799717
FrolSM <Frolov27101993@yandex.ru>
2026-03-12 18:57:46 +0300

    Изменение стилей отображения медиа контента и добавление увеличение фото при клике на него

--- static/news/js/script.js
+++ static/news/js/script.js
@@ -5,3 +5,62 @@ navToggle.addEventListener('click', function() {
     navBar.classList.toggle('active');
     navToggle.classList.toggle('active');
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImg");
+
+    if (!modal || !modalImg) return;
+
+    document.querySelectorAll(".post-image").forEach(img => {
+        img.addEventListener("click", function () {
+            modal.style.display = "block";
+            modalImg.src = this.src;
+        });
+    });
+
+    modal.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+            document.querySelectorAll('.like-btn').forEach(function(button) {
+                button.addEventListener('click', function() {
+                    console.log('Клик по лайку');
+                    var url = button.dataset.url;
+                    var slug = button.dataset.postSlug;
+
+                    fetch(url, {
+                        method: 'POST',
+                        headers: {
+                            'X-CSRFToken': getCookie('csrftoken')
+                        }
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                        button.textContent = data.liked ? 'Убрать лайк' : 'Лайк';
+                        document.querySelector(`.like-count[data-post-slug="${slug}"]`).textContent = data.count;
+                    })
+                    .catch(console.error);
+                });
+            });
+        });
+
+        // Функция для получения csrf из cookie
+        function getCookie(name) {
+            let cookieValue = null;
+            if (document.cookie && document.cookie !== '') {
+                let cookies = document.cookie.split(';');
+                for (let cookie of cookies) {
+                    cookie = cookie.trim();
+                    if (cookie.startsWith(name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
