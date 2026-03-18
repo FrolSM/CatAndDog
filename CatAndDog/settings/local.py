9e304a3c00d8ba64e198c928e819e02ae59ed27d
FrolSM <Frolov27101993@yandex.ru>
2026-03-18 22:25:07 +0300

    test(settings): добавил настройки CACHES для локального окружения

--- CatAndDog/settings/local.py
+++ CatAndDog/settings/local.py
@@ -5,12 +5,23 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_LOGIN'),
         'PASSWORD': os.getenv('DB_PASS'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://localhost:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        },
+        'KEY_PREFIX':'redis_cache'
     }
 }
 
